89811ff2bdf5a399677a9c78fc9b869e32d1ab22
Andsirpa <andsirpa94@gmail.com>
2024-04-18 17:27:43 +0200

    feat: add project info in App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,19 @@ export default {
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <div class="container">
+    <h1>{{ title }}</h1>
+
+    <div v-for="project in projects">
+      <ul>
+        <li><strong>ID: </strong> {{ project.id }}</li>
+        <li><strong>Title: </strong> {{ project.title }}</li>
+        <li><strong>Author: </strong> {{ project.author }}</li>
+        <li><strong>Description: </strong> {{ project.description }}</li>
+      </ul>
+
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
